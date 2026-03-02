fbc6f3d8d542254bf14d770084b2d51a99e0ebe2
riddhi2409 <riddhiagrawal0609@gmail.com>
2026-03-02 08:47:30 +0530

    initial commit

---
+++ src/main/java/com/ai/Resume/analyser/service/failureHandler.java
@@ -0,0 +1,21 @@
+package com.ai.Resume.analyser.service;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class failureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write("{\"error\":\"Authentication failed\"}");
+    }
+}
