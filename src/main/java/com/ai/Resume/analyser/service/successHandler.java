ef8001909856624bc0c1331f742b154f526c54a9
Riddhi2409 <98161523+Riddhi2409@users.noreply.github.com>
2026-03-03 10:04:07 +0530

    Update successHandler.java

--- src/main/java/com/ai/Resume/analyser/service/successHandler.java
+++ src/main/java/com/ai/Resume/analyser/service/successHandler.java
@@ -63,7 +63,7 @@ public class successHandler implements AuthenticationSuccessHandler {
         System.out.println("COOKIE SENT:  from successHandler: " + cookie.toString());
 
         // Return JSON instead of redirect
-         String frontendUrl = "https://localhost:5173/" + token;
+         String frontendUrl = "https://localhost:5173";
 
     response.sendRedirect(frontendUrl);
 
